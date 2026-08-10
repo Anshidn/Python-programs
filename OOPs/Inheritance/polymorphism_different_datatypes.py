9411f6215367a4b9597cea4d7e1b89794f0a69dc
Muhammed Anshid <shaluanshid57@gmail.com>
2026-08-10 16:19:15 +0530

    polymorphism length of different datatypes

---
+++ OOPs/Inheritance/polymorphism_different_datatypes.py
@@ -0,0 +1,10 @@
+def check_length(data):
+    print("length : ", len(data))
+
+string_eg = "Anshid"
+list_eg = [10,20,30]
+tuple_eg =(50,60,70)
+
+check_length(string_eg)
+check_length(list_eg)
+check_length(tuple_eg)
