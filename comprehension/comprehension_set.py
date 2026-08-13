443b1bd96b2eb4a9cff90e2ffa47e6247aa4b036
Muhammed Anshid <shaluanshid57@gmail.com>
2026-08-13 16:24:00 +0530

    comprehension dictionary

--- comprehension/comprehension_set.py
+++ comprehension/comprehension_set.py
@@ -1,4 +1,6 @@
-numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers = [1,4,5,4,7,2,5,2,3,4,5,6,7,8,9,10]
 unique_numbers = {x for x in numbers}
-print(unique_numbers)
 
+even_numbers = {x for x in numbers if x%2==0}
+print(unique_numbers)
+print(even_numbers)
---
+++ groccery_billing_system.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+cart=[]
+
+GST_RATE = 0.05
+
+def add_items():
+    print("/n-----Add items-----")
+
+    item_name=input("Enter item name :").strip()
+
