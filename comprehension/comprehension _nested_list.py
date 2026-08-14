c7318d2e4a7f785fc6cc9d358a0b2b0ca30fd7e3
Muhammed Anshid <shaluanshid57@gmail.com>
2026-08-14 12:35:24 +0530

    comprehension pair list

--- comprehension/comprehension _nested_list.py
+++ comprehension/comprehension _nested_list.py
@@ -1,5 +1,6 @@
 numbers=[[1,2],[3,4],[5,6]]
-print(numbers)
-
 combined=[num for row in numbers  for num in row ]
 print(combined) 
+
+pair=[(x,y) for x in range(1,3) for y in range(1,3)]
+print(pair) 
